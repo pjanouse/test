ee637653972fe4a6cab37c1186cbdc0be9c5de9e
Pavel Slavicek <pslavice@redhat.com>
2012-10-19 10:19:01 +0200

    LODH1 ignore exception when queue is already defined

--- jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/apps/debugtools/StandaloneClient.java
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/apps/debugtools/StandaloneClient.java
@@ -1,6 +1,9 @@
 package org.jboss.qa.hornetq.apps.debugtools;
 
 import org.jboss.qa.hornetq.apps.clients.SimpleJMSClient;
+import org.jboss.qa.hornetq.apps.clients.SubscriberAutoAck;
+import org.jboss.qa.hornetq.apps.clients.TopicClientsAutoAck;
+import org.jboss.qa.hornetq.test.HornetQTestCaseConstants;
 
 import javax.jms.Session;
 
@@ -10,10 +13,22 @@ import javax.jms.Session;
 public class StandaloneClient {
     public static void main(String[] args) {
         try {
+            /**
             SimpleJMSClient client = new SimpleJMSClient("127.0.0.1", 1099, 30, Session.AUTO_ACKNOWLEDGE, false);
             client.setInitialContextClass("org.jnp.interfaces.NamingContextFactory");
             client.setConnectionFactoryName("/ConnectionFactory");
-            client.sendMessages("InQueue");
+            client.sendMessages("TestTopic");
+             ***/              /**
+            SubscriberAutoAck topicClient = new SubscriberAutoAck(
+                    HornetQTestCaseConstants.EAP5_CONTAINER,
+                    "localhost", 1099, "testXTopic0", "id1", "name1");
+            topicClient.subscribe();
+
+            topicClient = new SubscriberAutoAck(
+                    HornetQTestCaseConstants.EAP5_CONTAINER,
+                    "localhost", 1099, "testXTopic0", "id2", "name2");
+            topicClient.subscribe();
+             **/
         } catch (Exception e) {
             e.printStackTrace();
         }
