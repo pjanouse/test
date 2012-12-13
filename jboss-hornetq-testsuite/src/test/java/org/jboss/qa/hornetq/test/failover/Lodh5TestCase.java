d97527cc871422bf88af3d2da4e46c9933bcc673
mnovak <mnovak@redhat.com>
2012-12-13 16:24:44 +0100

    modify lodh 5 change number of messages

--- jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/failover/Lodh5TestCase.java
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/failover/Lodh5TestCase.java
@@ -38,7 +38,7 @@ public class Lodh5TestCase extends HornetQTestCase {
 
     private static final Logger logger = Logger.getLogger(Lodh5TestCase.class);
     // this is just maximum limit for producer - producer is stopped once failover test scenario is complete
-    private static final int NUMBER_OF_MESSAGES_PER_PRODUCER = 1000;
+    private static final int NUMBER_OF_MESSAGES_PER_PRODUCER = 100000;
     // queue to send messages in 
     static String inQueueHornetQName = "InQueue";
     static String inQueueRelativeJndiName = "jms/queue/" + inQueueHornetQName;
