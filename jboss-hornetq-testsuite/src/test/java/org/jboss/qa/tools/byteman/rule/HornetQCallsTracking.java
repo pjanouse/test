1388486f754040a6ebeb16e6f1f43e85223c523d
Martin Svehla <msvehla@redhat.com>
2013-07-04 09:57:54 +0200

    Added helper class and byteman rules for tracking method calls.
    You can now use method HornetQCallsTracking.installTrackingRules() to track
    various method calls during test execution. Method allows to specify
    which kind of rules should be tracked.
    
    Byteman definition now mostly contain transaction and XA resources
    method tracking.

---
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/tools/byteman/rule/HornetQCallsTracking.java
@@ -0,0 +1,61 @@
+package org.jboss.qa.tools.byteman.rule;
+
+
+/**
+ * Utility class for helping with method calls tracking inside HornetQ server.
+ *
+ * @author <a href="mailto:msvehla@redhat.com">Martin Svehla</a>
+ */
+public final class HornetQCallsTracking {
+
+    public static final String CLIENT_SESSION_XA_RESOURCE_RULES = "byteman/client-session.btm";
+
+    public static final String HORNETQ_CORE_TRANSACTION_RULES = "byteman/hornetq-core-transaction.btm";
+
+    public static final String HORNETQ_RA_XA_RESOURCE_RULES = "byteman/hornetq-ra-xa-resource.btm";
+
+    public static final String HORNETQ_XA_RESOURCE_WRAPPER_RULES = "byteman/hornetq-xa-resource-wrapper.btm";
+
+    public static final String JOURNAL_RULES = "byteman/journal.btm";
+
+    public static final String STORE_MANAGER_RULES = "byteman/journal-storage-manager.btm";
+
+    public static final String TRANSACTION_MANAGER_RULES = "byteman/transaction-manager.btm";
+
+    // This ruleset is not active by default, beacause it generates errors with JDBC XA datasources
+    public static final String XA_RESOURCE_RULES = "byteman/xa-resource.btm";
+
+    private static final String[] ALL_RULE_FILES = new String[]{
+        CLIENT_SESSION_XA_RESOURCE_RULES,
+        HORNETQ_CORE_TRANSACTION_RULES,
+        HORNETQ_RA_XA_RESOURCE_RULES,
+        HORNETQ_XA_RESOURCE_WRAPPER_RULES,
+        JOURNAL_RULES,
+        STORE_MANAGER_RULES,
+        TRANSACTION_MANAGER_RULES
+    };
+
+
+    private HornetQCallsTracking() {
+    }
+
+
+    public static void installTrackingRules(final String host, final int port) {
+        installTrackingRules(host, port, ALL_RULE_FILES);
+    }
+
+
+    public static void installTrackingRules(final String host, final int port, final String... rulesFiles) {
+        SubmitUtil.host = host;
+        SubmitUtil.port = port;
+
+        for (String file : rulesFiles) {
+            try {
+                SubmitUtil.installFromStream(HornetQCallsTracking.class.getClassLoader().getResourceAsStream(file));
+            } catch (SubmitException e) {
+                throw new SubmitException("Cannot load rules from file " + file, e);
+            }
+        }
+    }
+
+}
