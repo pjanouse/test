1223f588a772fc6ebf35171a715b7e69c9f8d9bc
Martin Svehla <msvehla@redhat.com>
2013-07-17 13:12:51 +0200

    Added tests for 1way and 2way SSL connections

---
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/security/SecurityTestBase.java
@@ -0,0 +1,144 @@
+package org.jboss.qa.hornetq.test.security;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.hornetq.core.remoting.impl.netty.TransportConstants;
+import org.jboss.qa.hornetq.test.HornetQTestCase;
+import org.jboss.qa.hornetq.test.administration.AdministrationTestCase;
+import org.jboss.qa.tools.JMSOperations;
+
+
+/**
+ * Base class for security test cases.
+ *
+ * Contains methods for preparing connectors and acceptors over SSL and copying keystores
+ * to proper location.
+ *
+ * See {@link SslAuthenticationTestCase} for examples how to use it in tests.
+ *
+ * @author Martin Svehla &lt;msvehla@redhat.com&gt;
+ */
+public abstract class SecurityTestBase extends HornetQTestCase {
+
+    private static final String SOCKET_BINDING_NAME = "messaging";
+
+    private static final String TEST_KEYSTORES_DIRECTORY =
+            "src/test/resources/org/jboss/qa/hornetq/test/transportprotocols";
+
+    private static final String SERVER_KEYSTORE_DIR = JBOSS_HOME_1 + File.separator + "standalone" + File.separator
+            + "deployments" + File.separator;
+
+    protected static final String KEY_STORE_NAME = "hornetq.example.keystore";
+
+    protected static final String KEY_STORE_PATH = SERVER_KEYSTORE_DIR + KEY_STORE_NAME;
+
+    protected static final String KEY_STORE_PASSWORD = "hornetqexample";
+
+    protected static final String TRUST_STORE_NAME = "hornetq.example.truststore";
+
+    protected static final String TRUST_STORE_PATH = SERVER_KEYSTORE_DIR + TRUST_STORE_NAME;
+
+    protected static final String TRUST_STORE_PASSWORD = KEY_STORE_PASSWORD;
+
+
+    /**
+     * Copies keystore and truststore to JBOSS_HOME_1/standalone/deployments.
+     *
+     * Use {@link #KEY_STORE_PATH} and {@link #TRUST_STORE_PATH} to get proper paths.
+     *
+     * @throws IOException
+     */
+    protected void prepareServerSideKeystores() throws IOException {
+        File sourceKeystore = new File(TEST_KEYSTORES_DIRECTORY + File.separator + KEY_STORE_NAME);
+        File targetKeystore = new File(KEY_STORE_PATH);
+        this.copyFile(sourceKeystore, targetKeystore);
+
+        File sourceTruststore = new File(TEST_KEYSTORES_DIRECTORY + File.separator + TRUST_STORE_NAME);
+        File targetTruststore = new File(TRUST_STORE_PATH);
+        this.copyFile(sourceTruststore, targetTruststore);
+    }
+
+
+    protected void createOneWaySslConnector(final JMSOperations ops) {
+        this.createOneWaySslConnector("netty", ops);
+    }
+
+
+    protected void createOneWaySslConnector(final String connectorName, final JMSOperations ops) {
+        Map<String, String> props = new HashMap<String, String>();
+        props.put(TransportConstants.SSL_ENABLED_PROP_NAME, "true");
+        props.put(TransportConstants.KEYSTORE_PATH_PROP_NAME, KEY_STORE_PATH);
+        props.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME, KEY_STORE_PASSWORD);
+        this.createConnector(connectorName, ops, props);
+    }
+
+
+    protected void createOneWaySslAcceptor(final JMSOperations ops) {
+        this.createOneWaySslAcceptor("netty", ops);
+    }
+
+
+    protected void createOneWaySslAcceptor(final String acceptorName, final JMSOperations ops) {
+
+        Map<String, String> props = new HashMap<String, String>();
+        props.put(TransportConstants.SSL_ENABLED_PROP_NAME, "true");
+        props.put(TransportConstants.KEYSTORE_PATH_PROP_NAME, KEY_STORE_PATH);
+        props.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME, KEY_STORE_PASSWORD);
+        this.createAcceptor(acceptorName, ops, props);
+    }
+
+
+    protected void createTwoWaySslAcceptor(final JMSOperations ops) {
+        this.createTwoWaySslAcceptor("netty", ops);
+    }
+
+
+    protected void createTwoWaySslAcceptor(final String acceptorName, final JMSOperations ops) {
+        Map<String, String> props = new HashMap<String, String>();
+        props.put(TransportConstants.SSL_ENABLED_PROP_NAME, "true");
+        props.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME, TRUST_STORE_PATH);
+        props.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME, TRUST_STORE_PASSWORD);
+        props.put(TransportConstants.KEYSTORE_PATH_PROP_NAME, KEY_STORE_PATH);
+        props.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME, KEY_STORE_PASSWORD);
+        props.put(TransportConstants.NEED_CLIENT_AUTH_PROP_NAME, "true");
+        this.createAcceptor(acceptorName, ops, props);
+    }
+
+
+    private void createConnector(final String connectorName, final JMSOperations ops,
+            final Map<String, String> properties) {
+
+        try {
+            ops.removeRemoteConnector(connectorName);
+        } catch (Exception e) {
+        }
+
+        ops.createRemoteConnector(connectorName, SOCKET_BINDING_NAME, properties);
+    }
+
+
+    private void createAcceptor(final String acceptorName, final JMSOperations ops,
+            final Map<String, String> properties) {
+
+        try {
+            ops.removeRemoteAcceptor(acceptorName);
+        } catch (Exception e) {
+        }
+
+        ops.createRemoteAcceptor(acceptorName, SOCKET_BINDING_NAME, properties);
+    }
+
+
+    private void copyFile(final File source, final File target) throws IOException {
+        if (!target.exists()) {
+            target.createNewFile();
+        }
+
+        AdministrationTestCase fileOps = new AdministrationTestCase();
+        fileOps.copyFile(source, target);
+    }
+
+}
---
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/security/SslAuthenticationTestCase.java
@@ -0,0 +1,298 @@
+package org.jboss.qa.hornetq.test.security;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.jms.Connection;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.hornetq.api.core.Message;
+import org.hornetq.api.core.TransportConfiguration;
+import org.hornetq.api.core.client.ClientConsumer;
+import org.hornetq.api.core.client.ClientMessage;
+import org.hornetq.api.core.client.ClientProducer;
+import org.hornetq.api.core.client.ClientSession;
+import org.hornetq.api.core.client.ClientSessionFactory;
+import org.hornetq.api.core.client.HornetQClient;
+import org.hornetq.api.core.client.ServerLocator;
+import org.hornetq.api.jms.HornetQJMSClient;
+import org.hornetq.api.jms.JMSFactoryType;
+import org.hornetq.core.remoting.impl.netty.NettyConnectorFactory;
+import org.hornetq.core.remoting.impl.netty.TransportConstants;
+import org.hornetq.jms.client.HornetQConnectionFactory;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.qa.tools.JMSOperations;
+import org.jboss.qa.tools.arquillina.extension.annotation.RestoreConfigBeforeTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+
+/**
+ * @author Martin Svehla &lt;msvehla@redhat.com&gt;
+ */
+@RunWith(Arquillian.class)
+public class SslAuthenticationTestCase extends SecurityTestBase {
+
+    private static final String TEST_USER = "user";
+
+    private static final String TEST_USER_PASSWORD = "user.456";
+
+    private static final String QUEUE_NAME = "test.queue";
+
+    private static final String QUEUE_JNDI_ADDRESS = "jms/test/queue";
+
+    private static final String TEST_MESSAGE_BODY = "test text";
+
+    private static final long CONSUMER_TIMEOUT = 10000;
+
+
+    @Before
+    public void stopServerBeforeReconfiguration() {
+        this.controller.stop(CONTAINER1);
+    }
+
+
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    public void testOneWaySslOverCore() throws Exception {
+        this.controller.start(CONTAINER1);
+        JMSOperations ops = this.prepareServer();
+        this.createOneWaySslAcceptor(ops);
+        this.prepareServerSideKeystores();
+        ops.close();
+        this.controller.stop(CONTAINER1);
+        this.controller.start(CONTAINER1);
+
+        ServerLocator locator = null;
+        try {
+            Map<String, Object> props = new HashMap<String, Object>();
+            props.put(TransportConstants.HOST_PROP_NAME, CONTAINER1_IP);
+            props.put(TransportConstants.PORT_PROP_NAME, 5445);
+            props.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
+            props.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME, TRUST_STORE_PATH);
+            props.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME, TRUST_STORE_PASSWORD);
+            TransportConfiguration config = new TransportConfiguration(NettyConnectorFactory.class.getCanonicalName(),
+                    props);
+
+            locator = HornetQClient.createServerLocatorWithoutHA(config);
+            ClientSessionFactory sf = locator.createSessionFactory();
+            ClientSession session = sf.createSession(TEST_USER, TEST_USER_PASSWORD, false, true, true, false,
+                    locator.getAckBatchSize());
+            session.createTemporaryQueue(QUEUE_JNDI_ADDRESS, QUEUE_NAME);
+
+            ClientProducer producer = session.createProducer(QUEUE_JNDI_ADDRESS);
+            ClientMessage msg = session.createMessage(Message.TEXT_TYPE, false, 0, System.currentTimeMillis(), (byte) 4);
+            msg.getBodyBuffer().writeString(TEST_MESSAGE_BODY);
+            producer.send(msg);
+
+            ClientConsumer consumer = session.createConsumer(QUEUE_NAME);
+            session.start();
+            Message received = consumer.receive(CONSUMER_TIMEOUT);
+
+            assertNotNull("Cannot consume test message", received);
+            assertEquals("Sent and received messages have different body",
+                    TEST_MESSAGE_BODY, received.getBodyBuffer().readString());
+
+            session.stop();
+            producer.close();
+            session.close();
+            sf.close();
+        } finally {
+            if (locator != null) {
+                locator.close();
+            }
+        }
+    }
+
+
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    public void testOneWaySslOverJms() throws Exception {
+        this.controller.start(CONTAINER1);
+        JMSOperations ops = this.prepareServer();
+        this.createOneWaySslAcceptor(ops);
+        ops.createQueue(QUEUE_NAME, QUEUE_JNDI_ADDRESS);
+        this.prepareServerSideKeystores();
+        ops.close();
+        this.controller.stop(CONTAINER1);
+        this.controller.start(CONTAINER1);
+
+        ServerLocator locator = null;
+        try {
+            Map<String, Object> props = new HashMap<String, Object>();
+            props.put(TransportConstants.HOST_PROP_NAME, CONTAINER1_IP);
+            props.put(TransportConstants.PORT_PROP_NAME, 5445);
+            props.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
+            props.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME, TRUST_STORE_PATH);
+            props.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME, TRUST_STORE_PASSWORD);
+            TransportConfiguration config = new TransportConfiguration(NettyConnectorFactory.class.getCanonicalName(),
+                    props);
+
+            HornetQConnectionFactory cf = HornetQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, config);
+            Connection connection = cf.createConnection(TEST_USER, TEST_USER_PASSWORD);
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue testQueue = session.createQueue(QUEUE_NAME);
+
+            MessageProducer producer = session.createProducer(testQueue);
+            TextMessage msg = session.createTextMessage(TEST_MESSAGE_BODY);
+            producer.send(msg);
+
+            connection.start();
+            MessageConsumer consumer = session.createConsumer(testQueue);
+            TextMessage received = (TextMessage) consumer.receive(10000L);
+            connection.stop();
+
+            assertNotNull("Cannot consume test message", received);
+            assertEquals("Sent and received messages have different body", TEST_MESSAGE_BODY, received.getText());
+
+            consumer.close();
+            producer.close();
+            session.close();
+            connection.close();
+            cf.close();
+        } finally {
+            if (locator != null) {
+                locator.close();
+            }
+        }
+    }
+
+
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    public void testTwoWaySslOverCore() throws Exception {
+        this.controller.start(CONTAINER1);
+        JMSOperations ops = this.prepareServer();
+        this.createTwoWaySslAcceptor(ops);
+        ops.createQueue(QUEUE_NAME, QUEUE_JNDI_ADDRESS);
+        this.prepareServerSideKeystores();
+        ops.close();
+        this.controller.stop(CONTAINER1);
+        this.controller.start(CONTAINER1);
+
+        ServerLocator locator = null;
+        try {
+            Map<String, Object> props = new HashMap<String, Object>();
+            props.put(TransportConstants.HOST_PROP_NAME, CONTAINER1_IP);
+            props.put(TransportConstants.PORT_PROP_NAME, 5445);
+            props.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
+            props.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME, TRUST_STORE_PATH);
+            props.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME, TRUST_STORE_PASSWORD);
+            props.put(TransportConstants.KEYSTORE_PATH_PROP_NAME, KEY_STORE_PATH);
+            props.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME, KEY_STORE_PASSWORD);
+            TransportConfiguration config = new TransportConfiguration(NettyConnectorFactory.class.getCanonicalName(),
+                    props);
+
+            HornetQConnectionFactory cf = HornetQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, config);
+            Connection connection = cf.createConnection(TEST_USER, TEST_USER_PASSWORD);
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue testQueue = session.createQueue(QUEUE_NAME);
+
+            MessageProducer producer = session.createProducer(testQueue);
+            TextMessage msg = session.createTextMessage(TEST_MESSAGE_BODY);
+            producer.send(msg);
+
+            connection.start();
+            MessageConsumer consumer = session.createConsumer(testQueue);
+            TextMessage received = (TextMessage) consumer.receive(10000L);
+            connection.stop();
+
+            assertNotNull("Cannot consume test message", received);
+            assertEquals("Sent and received messages have different body", TEST_MESSAGE_BODY, received.getText());
+
+            consumer.close();
+            producer.close();
+            session.close();
+            connection.close();
+            cf.close();
+        } finally {
+            if (locator != null) {
+                locator.close();
+            }
+        }
+    }
+
+
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    public void testTwoWaySslOverJms() throws Exception {
+        this.controller.start(CONTAINER1);
+        JMSOperations ops = this.prepareServer();
+        this.createTwoWaySslAcceptor(ops);
+        ops.createQueue(QUEUE_NAME, QUEUE_JNDI_ADDRESS);
+        this.prepareServerSideKeystores();
+        ops.close();
+        this.controller.stop(CONTAINER1);
+        this.controller.start(CONTAINER1);
+
+        ServerLocator locator = null;
+        try {
+            Map<String, Object> props = new HashMap<String, Object>();
+            props.put(TransportConstants.HOST_PROP_NAME, CONTAINER1_IP);
+            props.put(TransportConstants.PORT_PROP_NAME, 5445);
+            props.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
+            props.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME, TRUST_STORE_PATH);
+            props.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME, TRUST_STORE_PASSWORD);
+            props.put(TransportConstants.KEYSTORE_PATH_PROP_NAME, KEY_STORE_PATH);
+            props.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME, KEY_STORE_PASSWORD);
+            TransportConfiguration config = new TransportConfiguration(NettyConnectorFactory.class.getCanonicalName(),
+                    props);
+
+            HornetQConnectionFactory cf = HornetQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, config);
+            Connection connection = cf.createConnection(TEST_USER, TEST_USER_PASSWORD);
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue testQueue = session.createQueue(QUEUE_NAME);
+
+            MessageProducer producer = session.createProducer(testQueue);
+            TextMessage msg = session.createTextMessage(TEST_MESSAGE_BODY);
+            producer.send(msg);
+
+            connection.start();
+            MessageConsumer consumer = session.createConsumer(testQueue);
+            TextMessage received = (TextMessage) consumer.receive(10000L);
+            connection.stop();
+
+            assertNotNull("Cannot consume test message", received);
+            assertEquals("Sent and received messages have different body", TEST_MESSAGE_BODY, received.getText());
+
+            consumer.close();
+            producer.close();
+            session.close();
+            connection.close();
+            cf.close();
+        } finally {
+            if (locator != null) {
+                locator.close();
+            }
+        }
+    }
+
+
+    private JMSOperations prepareServer() throws IOException {
+        JMSOperations ops = this.getJMSOperations();
+        ops.setPersistenceEnabled(true);
+
+        AddressSecuritySettings.forDefaultContainer(this)
+                .forAddress("jms.queue.test.#")
+                .giveUserAllPermissions(TEST_USER)
+                .create();
+
+        UsersSettings.forDefaultEapServer()
+                .withUser(TEST_USER, TEST_USER_PASSWORD, TEST_USER)
+                .create();
+
+        return ops;
+    }
+
+}
