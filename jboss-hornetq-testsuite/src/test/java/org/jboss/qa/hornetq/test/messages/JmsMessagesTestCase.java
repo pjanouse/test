2f2714c95d03d79c11d3868dcdb4dd70f0a66020
mnovak <mnovak@redhat.com>
2014-09-23 16:19:10 +0200

    test scheduled messages in diverts

--- jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/messages/JmsMessagesTestCase.java
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/messages/JmsMessagesTestCase.java
@@ -5,6 +5,7 @@ import org.hornetq.core.message.impl.MessageImpl;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.qa.hornetq.HornetQTestCase;
+import org.jboss.qa.hornetq.apps.impl.TextMessageBuilder;
 import org.jboss.qa.hornetq.test.categories.FunctionalTests;
 import org.jboss.qa.hornetq.tools.JMSOperations;
 import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.CleanUpBeforeTest;
@@ -36,6 +37,8 @@ public class JmsMessagesTestCase extends HornetQTestCase {
 
     private String inQueue = "InQueue";
     private String inQueueJndiName = "jms/queue/" + inQueue;
+    private String outQueue = "OutQueue";
+    private String outQueueJndiName = "jms/queue/" + outQueue;
 
     @Before
     public void startTestContainer() {
@@ -100,6 +103,107 @@ public class JmsMessagesTestCase extends HornetQTestCase {
         Assert.assertEquals("There must be 0 messages in queue.", 0, count);
         stopServer(CONTAINER1);
 
+    }
+
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    @CleanUpBeforeTest
+    public void testThatDivertedMessagesIsAlsoScheduledExclusive() throws Exception {
+        testThatDivertedMessagesIsAlsoScheduled(true, false);
+    }
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    @CleanUpBeforeTest
+    public void testThatDivertedMessagesIsAlsoScheduledExclusiveLargeMessage() throws Exception {
+        testThatDivertedMessagesIsAlsoScheduled(true, true);
+    }
+
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    @CleanUpBeforeTest
+    public void testThatDivertedMessagesIsAlsoScheduledNonExclusive() throws Exception {
+        testThatDivertedMessagesIsAlsoScheduled(false, false);
+    }
+
+    @Test
+    @RunAsClient
+    @RestoreConfigBeforeTest
+    @CleanUpBeforeTest
+    public void testThatDivertedMessagesIsAlsoScheduledNonExclusiveLargeMessage() throws Exception {
+        testThatDivertedMessagesIsAlsoScheduled(false, true);
+    }
+
+    private void testThatDivertedMessagesIsAlsoScheduled(boolean isExclusive, boolean isLargeMessage) throws Exception {
+
+        controller.start(CONTAINER1);
+
+        prepareServerWithDivert(CONTAINER1, inQueue, outQueue, isExclusive);
+
+        // send scheduled message
+        Context ctx = null;
+        Connection connection = null;
+        Session session = null;
+        try {
+            ctx = this.getContext(CONTAINER1);
+            ConnectionFactory cf = (ConnectionFactory) ctx.lookup(this.getConnectionFactoryName());
+            Queue originalQueue = (Queue) ctx.lookup(inQueueJndiName);
+            connection = cf.createConnection();
+            connection.start();
+
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            MessageProducer producer = session.createProducer(originalQueue);
+            TextMessage msg;
+            if (isLargeMessage)  {
+                msg = (TextMessage) new TextMessageBuilder(1024 * 1024).createMessage(session);
+            } else {
+                msg = (TextMessage) new TextMessageBuilder(1).createMessage(session);
+            }
+
+            long timeout = System.currentTimeMillis() + 5000;
+            msg.setLongProperty(MessageImpl.HDR_SCHEDULED_DELIVERY_TIME.toString(), timeout);
+
+            producer.send(msg);
+            producer.close();
+
+            Queue divertedQueue = (Queue) ctx.lookup(outQueueJndiName);
+            MessageConsumer consumerDiverted = session.createConsumer(divertedQueue);
+            Message receivedMessage = consumerDiverted.receive(1000);
+            Assert.assertNull("This is scheduled message which should not be received so soon.", receivedMessage);
+
+            if (!isExclusive)   {
+                MessageConsumer consumerOriginal = session.createConsumer(originalQueue);
+                receivedMessage = consumerOriginal.receive(1000);
+                Assert.assertNull("This is scheduled message which should not be received so soon.", receivedMessage);
+                receivedMessage = consumerOriginal.receive(10000);
+                Assert.assertNotNull("This is scheduled message which should be received now.", receivedMessage);
+            }
+
+            receivedMessage = consumerDiverted.receive(10000);
+            Assert.assertNotNull("This is scheduled message which should be received now.", receivedMessage);
+
+
+
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+
+            if (connection != null) {
+                connection.stop();
+                connection.close();
+            }
+
+            if (ctx != null) {
+                ctx.close();
+            }
+        }
+
+        stopServer(CONTAINER1);
 
     }
 
@@ -111,6 +215,17 @@ public class JmsMessagesTestCase extends HornetQTestCase {
 
         jmsOperations.close();
 
+
+    }
+
+    private void prepareServerWithDivert(String containerName, String originalQueue, String divertedQueue, boolean isExclusive)  {
+        JMSOperations jmsOperations = getJMSOperations(containerName);
+
+        jmsOperations.createQueue(inQueue, inQueueJndiName);
+        jmsOperations.createQueue(outQueue, outQueueJndiName);
+        jmsOperations.addDivert("myDivert", "jms.queue." + originalQueue, "jms.queue." + divertedQueue, isExclusive, null, null, null);
+
+        jmsOperations.close();
 
     }
 
