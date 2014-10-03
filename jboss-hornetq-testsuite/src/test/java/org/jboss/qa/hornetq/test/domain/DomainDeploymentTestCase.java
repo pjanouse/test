5221f4d5299840b109b5287857be50cdb0d4272a
Martin Svehla <msvehla@redhat.com>
2014-10-03 12:36:28 +0200

    Simple test case that demonstrates domain usage in tests

--- jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/domain/DomainDeploymentTestCase.java
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/domain/DomainDeploymentTestCase.java
@@ -6,15 +6,20 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.qa.hornetq.HornetQTestCase;
+import org.jboss.qa.hornetq.tools.DomainOperations;
+import org.jboss.qa.hornetq.tools.HornetQAdminOperationsEAP6;
+import org.jboss.qa.hornetq.tools.JMSOperations;
+import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.CleanUpBeforeTest;
+import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.RestoreConfigBeforeTest;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
-public class DomainDeploymentTestCase {
+public class DomainDeploymentTestCase { // extends HornetQTestCase {
 
     @ArquillianResource
     private ContainerController controller;
@@ -22,32 +27,63 @@ public class DomainDeploymentTestCase {
     @ArquillianResource
     private Deployer deployer;
 
-    @Deployment(name = "test-domain-deployment", managed = false)
-    @TargetsContainer("backend")
-    public static WebArchive createWebArchive() {
+    @Deployment(name = "server-group-deployment", managed = false)
+    @TargetsContainer("server-group-1")
+    public static WebArchive createServerGroupWebArchive() {
         return ShrinkWrap.create(WebArchive.class);
     }
 
-//    @Test
-//    @InSequence(1)
-//    @RunAsClient
-//    public void startServer() throws Exception {
-//        controller.start("backend-2");
-//    }
-//
-//    @Test
-//    @InSequence(2)
-//    @TargetsContainer("backend-2")
-//    public void testInContainer() throws Exception {
-//        System.out.println("in container");
+//    @Deployment(name = "server-deployment", managed = false)
+//    @TargetsContainer("master:server-1")
+//    public static WebArchive createServerWebArchive() {
+//        return ShrinkWrap.create(WebArchive.class);
 //    }
 
+    @Deployment(name = "node-deployment", managed = false)
+    @TargetsContainer("node-2")
+    public static WebArchive createNodeWebArchive() {
+        return ShrinkWrap.create(WebArchive.class);
+    }
+
     @Test
-    //@TargetsContainer("node-1")
+    @RunAsClient
+    @CleanUpBeforeTest
+    @RestoreConfigBeforeTest
     public void testDeploymentNode1() throws Exception {
-        controller.start("backend-2");
-        deployer.deploy("test-domain-deployment");
-        controller.stop("backend-2");
+        assert controller.isStarted("server-group-1");
+
+        DomainOperations domainOps = DomainOperations.forDefaultContainer();
+        // remove the default nodes/server groups
+        domainOps.removeServer("server-1");
+        domainOps.removeServer("server-2");
+
+        domainOps.createServer("server-1", "server-group-1");
+        domainOps.createServer("server-2", "server-group-1", 20000);
+        domainOps.close();
+
+        HornetQAdminOperationsEAP6 eap6AdmOps = new HornetQAdminOperationsEAP6();
+        eap6AdmOps.setHostname("localhost");
+        eap6AdmOps.setPort(9999);
+        eap6AdmOps.connect();
+
+        JMSOperations ops = eap6AdmOps;
+        ops.addAddressPrefix("profile", "full-ha-1");
+        ops.createQueue("TestQueue", "jms/queue/TestQUeue");
+        ops.close();
+
+        controller.start("node-1");
+        controller.start("node-2");
+
+        deployer.deploy("server-group-deployment");
+//        deployer.deploy("server-deployment");
+//        deployer.deploy("node-deployment");
+
+//        deployer.undeploy("node-deployment");
+//        deployer.undeploy("server-deployment");
+        //deployer.undeploy("server-group-deployment");
+
+        controller.stop("node-2");
+        controller.stop("node-1");
     }
 
 }
