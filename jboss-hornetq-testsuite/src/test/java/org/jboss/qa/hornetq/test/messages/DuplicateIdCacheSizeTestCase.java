0ddee9f70f1d5829055f8f96a326764a4ed62176
mnovak <mnovak@redhat.com>
2014-11-11 15:41:05 +0100

    modify test with dup ids and temp topic - todo - finish it

--- jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/messages/DuplicateIdCacheSizeTestCase.java
+++ jboss-hornetq-testsuite/src/test/java/org/jboss/qa/hornetq/test/messages/DuplicateIdCacheSizeTestCase.java
@@ -9,7 +9,6 @@ import org.jboss.qa.hornetq.tools.JMSOperations;
 import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.CleanUpBeforeTest;
 import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.RestoreConfigBeforeTest;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.jms.*;
@@ -19,6 +18,9 @@ import javax.naming.Context;
  * Created by mnovak on 9/22/14.
  *
  * Test whether there is dup id cache leak when queue is created and deleted many times.
+ *
+ * This is basically JMS request/reply with JMS standalone clients.
+ *
  */
 public class DuplicateIdCacheSizeTestCase extends HornetQTestCase {
 
@@ -27,20 +29,22 @@ public class DuplicateIdCacheSizeTestCase extends HornetQTestCase {
     private String inQueue = "InQueue";
     private String inQueueJndiName = "jms/queue/" + inQueue;
 
+    /**
+     * Start producer
+     * @throws Exception
+     */
     @Test
     @RunAsClient
     @RestoreConfigBeforeTest
     @CleanUpBeforeTest
-    @Ignore
     public void testDupIdCacheSizeWithDurableQueues() throws Exception {
 
 //        prepareServer(CONTAINER1);
 //        prepareServer(CONTAINER2);
 
         controller.start(CONTAINER1);
-        controller.start(CONTAINER2);
         final long numberOfMessages = 300;
-        final int numberOfIterations = 300;
+        final int numberOfIterations = 300000;
 
         Context ctx = null;
         Connection connection = null;
@@ -54,10 +58,7 @@ public class DuplicateIdCacheSizeTestCase extends HornetQTestCase {
             session = connection.createSession(true, Session.SESSION_TRANSACTED);
 
             for (int iterations = 0; iterations < numberOfIterations; iterations++) {
-//                TemporaryQueue tempQueue = session.createTemporaryQueue();
-                createQueue(CONTAINER1, inQueue, inQueueJndiName);
-                createQueue(CONTAINER2, inQueue, inQueueJndiName);
-                Queue tempQueue = (Queue) ctx.lookup(inQueueJndiName);
+                TemporaryQueue tempQueue = session.createTemporaryQueue();
                 MessageProducer producer = session.createProducer(tempQueue);
                 MessageBuilder messageBuilder = new TextMessageBuilder(1);
                 messageBuilder.setAddDuplicatedHeader(true);
@@ -81,9 +82,9 @@ public class DuplicateIdCacheSizeTestCase extends HornetQTestCase {
                 logger.info("Iteration: " + iterations + " Commit receive " + numberOfMessages + " from temp queue.");
                 Assert.assertEquals(count, numberOfMessages);
                 consumer.close();
-                removeQueue(CONTAINER1, inQueue);
-                removeQueue(CONTAINER2, inQueue);
-//                tempQueue.delete();
+//                removeQueue(CONTAINER1, inQueue);
+//                removeQueue(CONTAINER2, inQueue);
+                tempQueue.delete();
             }
 
         } finally {
@@ -101,19 +102,6 @@ public class DuplicateIdCacheSizeTestCase extends HornetQTestCase {
             }
         }
         stopServer(CONTAINER1);
-        stopServer(CONTAINER2);
-    }
-
-    private void removeQueue(String containerName, String coreQueueName)    {
-        JMSOperations jmsOperations = getJMSOperations(containerName);
-        jmsOperations.removeQueue(coreQueueName);
-        jmsOperations.close();
-    }
-
-    private void createQueue(String containerName, String coreQueueName, String jndiQueueName)    {
-        JMSOperations jmsOperations = getJMSOperations(containerName);
-        jmsOperations.createQueue(coreQueueName, jndiQueueName);
-        jmsOperations.close();
     }
 
     private void prepareServer(String containerName) {
