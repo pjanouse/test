133155d0b4aeddab1e46cd9372d0c3519c4a9911
mnovak <mnovak@redhat.com>
2015-03-26 14:57:30 +0100

    SocketBindingAttributesTestCase

--- jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/cli/attributes/SocketBindingAttributesTestCase.java
+++ jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/cli/attributes/SocketBindingAttributesTestCase.java
@@ -13,35 +13,33 @@ import org.junit.experimental.categories.Category;
 
 /**
  * Created by okalman on 12/3/14.
- *
  */
 @Category(FunctionalTests.class)
 public class SocketBindingAttributesTestCase extends HornetQTestCase {
+
     private static final Logger logger = Logger.getLogger(SocketBindingAttributesTestCase.class);
 
     @Test
     @RunAsClient
     @CleanUpBeforeTest
     @RestoreConfigBeforeTest
-    public void bindingTest(){
-        container(1).start();
+    public void bindingTest() {
 
+        container(1).start();
         JMSOperations jmsOperations = container(1).getJmsOperations();
-        String result=jmsOperations.getSocketBindingAtributes("messaging");
-        String[]resultArr=result.split(",");
-        Assert.assertTrue("Socket bound attribute is false, should be true",resultArr[0].equals("\"bound\" => true"));
-        if(container(1).getHostname().contains(":")){
+        String result = jmsOperations.getSocketBindingAtributes("messaging");
+        String[] resultArr = result.split(",");
+        Assert.assertTrue("Socket bound attribute is false, should be true", resultArr[0].equals("\"bound\" => true"));
+
+        if (container(1).getHostname().contains(":")) {
             //for IPv6
-            Assert.assertTrue("Socket bound-address should be defined correctly", resultArr[1].contains(container(1).getHostname().substring(1,container(1).getHostname().length()-2)));
-        }else {
+            Assert.assertTrue("Socket bound-address should be defined correctly", resultArr[1].contains(container(1).getHostname().substring(1, container(1).getHostname().length() - 2)));
+        } else {
             //for IPv4
             Assert.assertTrue("Socket bound-address should be defined correctly", resultArr[1].equals("\"bound-address\" => \"" + container(1).getHostname() + "\""));
         }
-
-
         jmsOperations.close();
         container(1).stop();
-
     }
 
 }
