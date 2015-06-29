297f0efc08973beed03deff1ffdd26da0ae31a00
Martin Svehla <msvehla@redhat.com>
2015-06-29 14:37:01 +0200

    Add basic failover tests for domain mode in EAP6

--- jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/domain/DomainDeploymentTestCase.java
+++ jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/domain/DomainDeploymentTestCase.java
@@ -1,46 +1,52 @@
 package org.jboss.qa.hornetq.test.domain;
 
-import org.jboss.arquillian.container.test.api.ContainerController;
-import org.jboss.arquillian.container.test.api.Deployer;
-import org.jboss.arquillian.container.test.api.Deployment;
+import org.apache.log4j.Logger;
 import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.qa.hornetq.DomainContainer;
 import org.jboss.qa.hornetq.DomainHornetQTestCase;
-import org.jboss.qa.hornetq.tools.DomainOperations;
+import org.jboss.qa.hornetq.DomainServerGroup;
+import org.jboss.qa.hornetq.apps.mdb.LocalMdbFromQueue;
+import org.jboss.qa.hornetq.test.categories.DomainTests;
 import org.jboss.qa.hornetq.tools.JMSOperations;
 import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.CleanUpBeforeTest;
 import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.RestoreConfigBeforeTest;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
+@Category(DomainTests.class)
 public class DomainDeploymentTestCase extends DomainHornetQTestCase {
 
-    @ArquillianResource
-    private ContainerController controller;
+    private static final Logger LOG = Logger.getLogger(DomainDeploymentTestCase.class);
 
-    @ArquillianResource
-    private Deployer deployer;
+    // @ArquillianResource
+    // private ContainerController controller;
 
-    @Deployment(name = "server-group-deployment", managed = false)
-    @TargetsContainer("server-group-1")
-    public static WebArchive createServerGroupWebArchive() {
-        return ShrinkWrap.create(WebArchive.class);
+    // @ArquillianResource
+    // private Deployer deployer;
+
+    private final WebArchive SERVER_GROUP_DEPLOYMENT = createServerGroupWebArchive();
+
+    // @Deployment(name = "server-group-deployment", managed = false)
+    // @TargetsContainer("server-group-1")
+    private WebArchive createServerGroupWebArchive() {
+        return ShrinkWrap.create(WebArchive.class)
+                .addClass(LocalMdbFromQueue.class);
     }
 
-//    @Deployment(name = "server-deployment", managed = false)
-//    @TargetsContainer("master:server-1")
-//    public static WebArchive createServerWebArchive() {
-//        return ShrinkWrap.create(WebArchive.class);
-//    }
+    // @Deployment(name = "server-deployment", managed = false)
+    // @TargetsContainer("master:server-1")
+    // public static WebArchive createServerWebArchive() {
+    // return ShrinkWrap.create(WebArchive.class);
+    // }
 
-    @Deployment(name = "node-deployment", managed = false)
-    @TargetsContainer("node-2")
-    public static WebArchive createNodeWebArchive() {
+    // @Deployment(name = "node-deployment", managed = false)
+    // @TargetsContainer("node-2")
+    public WebArchive createNodeWebArchive() {
         return ShrinkWrap.create(WebArchive.class);
     }
 
@@ -48,40 +54,30 @@ public class DomainDeploymentTestCase extends DomainHornetQTestCase {
     @RunAsClient
     @CleanUpBeforeTest
     @RestoreConfigBeforeTest
-    public void testDeploymentNode1() throws Exception {
-        assert controller.isStarted("server-group-1");
+    public void testDeployment() throws Exception {
+        DomainContainer host = domainContainer();
 
-        DomainOperations domainOps = DomainOperations.forDefaultContainer();
-        // remove the default nodes/server groups
-        domainOps.removeServer("server-1");
-        domainOps.removeServer("server-2");
+        // delete server 1/2
+        host.serverGroup("server-group-2").deleteNode("server-2");
+        host.serverGroup("server-group-1").deleteNode("server-1");
 
-        domainOps.createServer("server-1", "server-group-1", container(1).getPortOffset());
-        domainOps.createServer("server-2", "server-group-1", container(2).getPortOffset());
-        domainOps.close();
+        // recreate both under server-group-1
+        host.serverGroup("server-group-1").createNode("server-1");
+        host.serverGroup("server-group-1").createNode("server-2", 1000);
+        host.reloadDomain();
 
-        JMSOperations eap6AdmOps = container(1).getJmsOperations();
-
-        JMSOperations ops = eap6AdmOps;
-        ops.addAddressPrefix("profile", "full-ha-1");
-        ops.createQueue("TestQueue", "jms/queue/TestQUeue");
+        DomainServerGroup serverGroup1 = host.serverGroup("server-group-1");
+        JMSOperations ops = serverGroup1.getJmsOperations();
+        ops.createQueue("InQueue", "jms/queue/InQueue");
+        ops.createQueue("OutQueue", "jms/queue/OutQueue");
         ops.close();
 
-        controller.start("node-1");
-        controller.start("node-2");
-
-        deployer.deploy("server-group-deployment");
-//        deployer.deploy("server-deployment");
-//        deployer.deploy("node-deployment");
-
-//        deployer.undeploy("node-deployment");
-//        deployer.undeploy("server-deployment");
-        deployer.undeploy("server-group-deployment");
-
-        controller.stop("node-2");
-        controller.stop("node-1");
-
-//        Thread.sleep(60000);
+        serverGroup1.startAllNodes();
+        serverGroup1.deploy(SERVER_GROUP_DEPLOYMENT);
+        Thread.sleep(5000);
+        serverGroup1.undeploy(SERVER_GROUP_DEPLOYMENT);
+        Thread.sleep(5000);
+        serverGroup1.stopAllNodes();
     }
 
 }
