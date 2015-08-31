00513fdc0ebcb5dea1acfb05d1942e663f50d498
Erich Duda <eduda@redhat.com>
2015-08-31 15:01:09 +0200

    RemoveJndiOperationTestCase: stop server after failed tests

--- jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/cli/attributes/RemoveJndiOperationTestCase.java
+++ jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/cli/attributes/RemoveJndiOperationTestCase.java
@@ -8,6 +8,7 @@ import org.jboss.qa.hornetq.test.categories.FunctionalTests;
 import org.jboss.qa.hornetq.tools.JMSOperations;
 import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.CleanUpBeforeTest;
 import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.RestoreConfigBeforeTest;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -43,6 +44,10 @@ public class RemoveJndiOperationTestCase extends HornetQTestCase {
     String topicJndiNameFullExported1 = "java:jboss/exported/" + topicJndiNameRelative1;
     String topicJndiNameFullExported2 = "java:jboss/exported/" + topicJndiNameRelative2;
 
+    @After
+    public void stopServer() {
+        container(1).stop();
+    }
 
     @Test
     @RunAsClient
