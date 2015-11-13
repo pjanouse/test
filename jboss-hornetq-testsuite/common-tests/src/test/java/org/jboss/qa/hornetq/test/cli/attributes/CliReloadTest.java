85b537b1f5a53bf7f882ed0cf614bf17be16183a
Miroslav Novak <mnovak@redhat.com>
2015-11-13 12:55:48 +0100

    Test reproducing jvm crash during reload

---
+++ jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/cli/attributes/CliReloadTest.java
@@ -0,0 +1,67 @@
+package org.jboss.qa.hornetq.test.cli.attributes;
+
+import org.apache.log4j.Logger;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.as.cli.scriptsupport.CLI;
+import org.jboss.qa.hornetq.HornetQTestCase;
+import org.jboss.qa.hornetq.test.categories.FunctionalTests;
+import org.jboss.qa.hornetq.tools.CheckServerAvailableUtils;
+import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.CleanUpBeforeTest;
+import org.jboss.qa.hornetq.tools.arquillina.extension.annotation.RestoreConfigBeforeTest;
+import org.jboss.qa.management.cli.CliConfiguration;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+/**
+ * Created by mnovak on 9/16/14.
+ * <p>
+ * Test remove Jndi operation.
+ */
+@Category(FunctionalTests.class)
+public class CliReloadTest extends HornetQTestCase {
+
+    private static final Logger logger = Logger.getLogger(CliReloadTest.class);
+
+    CliConfiguration cliConf = new CliConfiguration(container(1).getHostname(), container(1).getPort(), container(1).getUsername(), container(1).getPassword());
+
+    @Test
+    @RunAsClient
+    @CleanUpBeforeTest
+    @RestoreConfigBeforeTest
+    public void reloadServerTest() throws InterruptedException {
+
+        container(1). start();
+        for (int i = 0; i < 100; i++) {
+
+            try {
+                CLI cli = CLI.newInstance();
+                Thread.sleep(1000);
+                logger.info("!!!!!!!!!!!!!!!!!!! Connect !!!!!!!!!!!!!!!!!!!!!!");
+                cli.connect(cliConf.getHost(), cliConf.getPort(), cliConf.getUser(), cliConf.getPassword());
+                logger.info("!!!!!!!!!!!!!!!!!!! Reload !!!!!!!!!!!!!!!!!!!!!!");
+                cli.cmd("reload");
+                logger.info("!!!!!!!!!!!!!!!!!!! Disconnect !!!!!!!!!!!!!!!!!!!!!!");
+                cli.disconnect();
+                logger.info("!!!!!!!!!!!!!!!!!!! Done !!!!!!!!!!!!!!!!!!!!!!");
+
+                Assert.assertTrue(CheckServerAvailableUtils.waitForLiveServerToReload(cliConf.getHost(), cliConf.getPort(), 15000));
+            } catch (IllegalStateException e) {
+                // do nothing
+            }
+        }
+        container(1).stop();
+    }
+
+    @Test
+    @RunAsClient
+    @CleanUpBeforeTest
+    @RestoreConfigBeforeTest
+    public void justStartServerByArquillianAndWait() throws InterruptedException {
+        container(1).start();
+        logger.info("Server started for 60 minutes.");
+        Thread.sleep(60 * 60 * 1000);
+        container(1).stop();
+    }
+}
+
