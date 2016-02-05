f66347aaa5acc52d83422c5fc1f85698089a2189
Ondrej Kalman <okalman@redhat.com>
2016-02-05 08:42:01 +0100

    fixed rule names for HQ and AMQ

--- jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/failover/ColocatedClusterFailoverTestCase.java
+++ jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/failover/ColocatedClusterFailoverTestCase.java
@@ -120,19 +120,11 @@ public class ColocatedClusterFailoverTestCase extends HornetQTestCase {
      * @throws Exception
      */
     @BMRules({
-            @BMRule(name = "Kill server when a number of messages were received",
-                    targetClass = "org.hornetq.core.postoffice.impl.PostOfficeImpl",
-                    targetMethod = "processRoute",
-                    action = "System.out.println(\"Byteman - Killing server!!!\"); killJVM();"),
-            @BMRule(name = "Kill server when a number of messages were received",
+            @BMRule(name = "Kill server when a number of messages were received HQ",
                     targetClass = "org.apache.activemq.artemis.core.postoffice.impl.PostOfficeImpl",
                     targetMethod = "processRoute",
                     action = "System.out.println(\"Byteman - Killing server!!!\"); killJVM();"),
-            @BMRule(name = "Kill server when a number of messages were received",
-                    targetClass = "org.apache.activemq.artemis.core.postoffice.impl.PostOfficeImpl",
-                    targetMethod = "processRoute",
-                    action = "System.out.println(\"Byteman - Killing server!!!\"); killJVM();"),
-            @BMRule(name = "Kill server when a number of messages were received",
+            @BMRule(name = "Kill server when a number of messages were received AMQ",
                     targetClass = "org.apache.activemq.artemis.core.postoffice.impl.PostOfficeImpl",
                     targetMethod = "processRoute",
                     action = "System.out.println(\"Byteman - Killing server!!!\"); killJVM();")})
