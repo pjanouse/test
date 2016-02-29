72ba980ce43f60db5bd68c0472bf730e238e5466
Erich Duda <eduda@redhat.com>
2016-02-29 09:19:41 +0100

    RemoteConnectionFactoryTestCase: timeout increased to 10 min

--- jboss-hornetq-testsuite/tests-eap7/src/test/java/org/jboss/qa/artemis/test/cli/attributes/RemoteConnectionFactoryTestCase.java
+++ jboss-hornetq-testsuite/tests-eap7/src/test/java/org/jboss/qa/artemis/test/cli/attributes/RemoteConnectionFactoryTestCase.java
@@ -94,7 +94,7 @@ public class RemoteConnectionFactoryTestCase extends CliTestBase {
     private static final Logger logger = Logger.getLogger(RemoteConnectionFactoryTestCase.class);
 
     @Rule
-    public Timeout timeout = new Timeout(400000);
+    public Timeout timeout = new Timeout(600000);
 
     private Properties attributes;
 
