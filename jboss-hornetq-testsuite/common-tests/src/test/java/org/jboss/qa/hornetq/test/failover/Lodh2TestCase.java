9f11da2d12b45c351b3c29b512648395a6f7bcec
Miroslav Novak <mnovak@redhat.com>
2016-03-07 10:35:31 +0100

    Move waitForMessages behind waiting for prepared transactions in lodh2 test

--- jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/failover/Lodh2TestCase.java
+++ jboss-hornetq-testsuite/common-tests/src/test/java/org/jboss/qa/hornetq/test/failover/Lodh2TestCase.java
@@ -936,14 +936,14 @@ public class Lodh2TestCase extends HornetQTestCase {
 
         executeFailureSequence(failureSequence, 5000, failureType);
 
-        new JMSTools().waitForMessages(outQueueName, NUMBER_OF_MESSAGES_PER_PRODUCER, 300000, container(1), container(2),
-                container(3), container(4));
-
         new TransactionUtils().waitUntilThereAreNoPreparedHornetQTransactions(300000, container(1));
         new TransactionUtils().waitUntilThereAreNoPreparedHornetQTransactions(300000, container(2));
         new TransactionUtils().waitUntilThereAreNoPreparedHornetQTransactions(300000, container(3));
         new TransactionUtils().waitUntilThereAreNoPreparedHornetQTransactions(300000, container(4));
 
+        new JMSTools().waitForMessages(outQueueName, NUMBER_OF_MESSAGES_PER_PRODUCER, 300000, container(1), container(2),
+                container(3), container(4));
+        
         // set longer timeouts so xa recovery is done at least once
         ReceiverTransAck receiver1 = new ReceiverTransAck(outServer, outQueueJndiName, 30000, 10, 10);
         receiver1.setMessageVerifier(messageVerifier);
