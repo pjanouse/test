745d4a2f999325398fd36c4a73fad5ade07821c2
Erich Duda <eduda@redhat.com>
2016-04-15 17:06:49 +0200

    Changed version of clients in repeat_test_until_fail.sh

--- jboss-hornetq-testsuite/repeat_test_until_fail.sh
+++ jboss-hornetq-testsuite/repeat_test_until_fail.sh
@@ -3,7 +3,7 @@ for i in {1..50}
 do
         echo "Run test for the $i time."        
 
-        mvn clean test -Dtest=$1   -DfailIfNoTests=false  -Deap=7x -Deap7.org.jboss.qa.hornetq.apps.clients.version=7.0.0.CR1-with-all-fixes | tee log
+        mvn clean test -Dtest=$1   -DfailIfNoTests=false  -Deap=7x -Deap7.org.jboss.qa.hornetq.apps.clients.version=7.0.0.CR1-with-all-fixes-2 | tee log
 
         export GREP=`grep "Failures: 0, Errors: 0" log`
         echo $GREP
