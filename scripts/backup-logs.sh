37a31c51dafbd66ffdb036c62098450be251f96f
Erich Duda <eduda@redhat.com>
2016-06-06 16:34:47 +0200

    Update scripts/backup-logs.sh

--- scripts/backup-logs.sh
+++ scripts/backup-logs.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-env
-
 FILES=$(mktemp)
 
-find $WORKSPACE -name "target" >> $FILES
-find $WORKSPACE -name "test-suite.log" >> $FILES
-
-
 cd $WORKSPACE
-cat $FILES | while read LINE; do realpath --relative-to="$WORKSPACE" $LINE; done | xargs zip -q -r $WORKSPACE/logs.zip
+
+find . -name "target" >> $FILES
+find . -name "test-suite.log" >> $FILES
+
+
+cat $FILES | xargs zip -q -r logs.zip
 
 rm -f $FILES
