3fa8abf52d5ca08a3032846bee7aedc676f01ae7
Miroslav Novak <mnovak@redhat.com>
2016-08-30 16:50:13 +0200

    do not be quite when zipping logs

--- scripts/backup-logs.sh
+++ scripts/backup-logs.sh
@@ -11,7 +11,7 @@ find . -name "test-suite.log" >> $LOGS
 find . -name "*.xml" | grep "surefire-reports" >> $REPORTS
 
 cat $LOGS
-cat $LOGS | xargs zip -q -r logs.zip
+cat $LOGS | xargs zip -r logs.zip
 
 # zip log directories of servers 
 # this is good in case that build timeouted and we want to see the log directories of all servers
@@ -19,7 +19,7 @@ find -name log | grep jboss-eap | xargs zip -r logs.zip
 find -name "*thread-dump*" | grep jboss-eap | xargs zip -r logs.zip
 
 cat $REPORTS
-cat $REPORTS | xargs zip -q -r reports.zip
+cat $REPORTS | xargs zip -r reports.zip
 
 rm -f $LOGS
 rm -f $REPORTS
