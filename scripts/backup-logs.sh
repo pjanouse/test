0e632c442ba6e9c514c3d0c47bbba354eec39cef
Miroslav Novak <mnovak@redhat.com>
2016-08-30 16:29:35 +0200

    Also log thread dumps from unfished runs in - backup-logs

--- scripts/backup-logs.sh
+++ scripts/backup-logs.sh
@@ -16,6 +16,7 @@ cat $LOGS | xargs zip -q -r logs.zip
 # zip log directories of servers 
 # this is good in case that build timeouted and we want to see the log directories of all servers
 find -name log | grep jboss-eap | xargs zip -r logs.zip
+find -name "*thread-dump*" | grep jboss-eap | xargs zip -r logs.zip
 
 cat $REPORTS
 cat $REPORTS | xargs zip -q -r reports.zip
