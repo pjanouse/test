0ebf651bea2f2371d02c1d753b1abcd234c1dcd1
Miroslav Novak <mnovak@redhat.com>
2016-08-31 12:05:24 +0200

    Do not fail when thread dump not found

--- scripts/backup-logs.sh
+++ scripts/backup-logs.sh
@@ -8,15 +8,15 @@ cd $WORKSPACE
 find . -name "target" >> $LOGS
 find . -name "test-suite.log" >> $LOGS
 
+# zip log directories of servers 
+# this is good in case that build timeouted and we want to see the log directories of all servers
+find -name log | grep jboss-eap >> $LOGS
+find -name "*thread-dump*" | grep jboss-eap >> $LOGS
+
 find . -name "*.xml" | grep "surefire-reports" >> $REPORTS
 
 cat $LOGS
 cat $LOGS | xargs zip -r logs.zip
-
-# zip log directories of servers 
-# this is good in case that build timeouted and we want to see the log directories of all servers
-find -name log | grep jboss-eap | xargs zip -r logs.zip
-find -name "*thread-dump*" | grep jboss-eap | xargs zip -r logs.zip
 
 cat $REPORTS
 cat $REPORTS | xargs zip -r reports.zip
