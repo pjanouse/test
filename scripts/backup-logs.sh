a0fe754f277a141cdbb2310e11f295f581042e48
Miroslav Novak <mnovak@redhat.com>
2016-09-01 08:28:07 +0200

    find command needs . to not fail in jenkins in backup-logs.sh

--- scripts/backup-logs.sh
+++ scripts/backup-logs.sh
@@ -10,8 +10,8 @@ find . -name "test-suite.log" >> $LOGS
 
 # zip log directories of servers 
 # this is good in case that build timeouted and we want to see the log directories of all servers
-find -name log | grep jboss-eap >> $LOGS
-find -name "*thread-dump*" | grep jboss-eap >> $LOGS
+find . -name log | grep jboss-eap >> $LOGS
+find . -name "*thread-dump*" | grep jboss-eap >> $LOGS
 
 find . -name "*.xml" | grep "surefire-reports" >> $REPORTS
 
